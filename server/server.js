e532442ca1b0e64a12afcc30df8568d13e4feaf4
Paul Molluzzo <paul@molluzzo.com>
2014-04-26 14:31:03 -0400

    remove npm package and related code

--- server/server.js
+++ server/server.js
@@ -1,27 +1,5 @@
 var twitter = new TwitterApi();
 
-// var conf = JSON.parse(Assets.getText('twitter.json'));
-
-// Meteor.startup(function() {
-//   Accounts.loginServiceConfiguration.remove({
-//     service: "twitter"
-//   });
-//   Accounts.loginServiceConfiguration.insert({
-//     service: "twitter",
-//     consumerKey: conf.consumer.key,
-//     secret: conf.consumer.secret
-//   });
-// });
-
-// Future = Npm.require('fibers/future');
-// var Twitter = Meteor.require("twitter");
-// var twit = new Twitter({
-// 	consumer_key: conf.consumer.key,
-// 	consumer_secret: conf.consumer.secret,
-// 	access_token_key: conf.access_token.key,
-// 	access_token_secret: conf.access_token.secret
-// });
-
 Meteor.methods({
     searchTwitter: function(term) {
         return twitter.search(term);
@@ -29,9 +7,5 @@ Meteor.methods({
 
     getUser: function (term) {
         return twitter.get('users/show.json', {screen_name: term});
-    },
-
-    newTest: function(term) {
-        return twitter.get('/users/show.json',{screen_name: term})
     }
 })
