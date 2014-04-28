89d089185f7700903bd9f15fbefaf314d626c7b4
Paul Molluzzo <paul@molluzzo.com>
2014-04-28 13:35:47 -0400

    Update style for search

--- client/search_tweets/search_tweets.js
+++ client/search_tweets/search_tweets.js
@@ -1,17 +1,17 @@
 Template.searchTweets.events({
-    'click #search': function() {
+    'submit #search-tweets': function(event) {
+        event.preventDefault();
         Tweets.remove({});
         var term = document.getElementById('search-term').value || "test";
-        document.getElementById('search').disabled = true;
         Meteor.call('searchTwitter', term, function(err, result){
             if(!err){
                 result.headers.date = moment(result.headers.date).toDate();
                 for (var i=0; i < result.data.statuses.length; i++) {
                     Tweets.insert(result.data.statuses[i]);  
                 }
+                console.log("done");
             }
         });
-        document.getElementById('search').disabled = false;
         return Tweets.find({});
     }
 })
