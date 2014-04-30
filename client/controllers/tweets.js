0b094be00be49eac502a19ba973876961e06b0e0
Paul Molluzzo <paul@molluzzo.com>
2014-04-30 14:36:51 -0400

    Fix typo in controller that prevented deployment

--- client/controllers/tweets.js
+++ client/controllers/tweets.js
@@ -2,34 +2,6 @@ Template.tweetLoop.tweets = function() {
 	return Tweets.find({});
 };
 
-Template.TweetLoop.isPhoto = function() {
-    return this.type === "photo";
-};
-
-Template.postTweet.tweet = function() {
-    return Tweets.findOne({});
-};
-
-Template.homeTimeline.tweets = function() {
-    return Tweets.find({});
-};
-
-Template.homeTimeline.isPhoto = function() {
-    return this.type === "photo";
-};
-
-Template.userTimeline.tweets = function() {
-    return Tweets.find({});
-};
-
-Template.userTimeline.isPhoto = function() {
-    return this.type === "photo";
-};
-
-Template.mentionTimeline.tweets = function() {
-    return Tweets.find({});
-};
-
-Template.mentionTimeline.isPhoto = function() {
+Template.tweetLoop.isPhoto = function() {
     return this.type === "photo";
 };
